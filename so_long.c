61e12158d9c3b90f338def40e0078a9ce5c4f027
Pierre SZLEPER <pszleper@e3r8p10.clusters.42paris.fr>
2022-06-17 22:09:00 +0200

    Added error checking to mlx functions in main, created ft_close function to shut down the display and game

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: pszleper <pszleper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 18:39:53 by pszleper          #+#    #+#             */
-/*   Updated: 2022/06/17 20:47:28 by pszleper         ###   ########.fr       */
+/*   Updated: 2022/06/17 22:07:41 by pszleper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,22 @@ int	main()
 	void	*mlx_win;
 
 	mlx_ptr = mlx_init();
+	if (mlx_ptr == NULL)
+		return (MLX_ERROR);
 	mlx_win = mlx_new_window(mlx_ptr, 1920, 1080, "So_long");
-	(void) mlx_win;
+	if (mlx_win == NULL)
+	{
+		free(mlx_ptr);
+		return (MLX_ERROR);
+	}
 	sleep(2);
-	ft_close(mlx_ptr, mlx_win);
+	ft_close(&mlx_ptr, &mlx_win);
 	return (0);
 }
 
-void	ft_close(void *mlx_ptr, void *mlx_win)
+void	ft_close(void **mlx_ptr, void **mlx_win)
 {
-	mlx_destroy_window(mlx_ptr, mlx_win);
-	mlx_destroy_display(mlx_ptr);
-	free(mlx_ptr);
+	mlx_destroy_window(*mlx_ptr, *mlx_win);
+	mlx_destroy_display(*mlx_ptr);
+	free(*mlx_ptr);
 }
--- so_long.h
+++ so_long.h
@@ -6,18 +6,19 @@
 /*   By: pszleper <pszleper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 18:54:17 by pszleper          #+#    #+#             */
-/*   Updated: 2022/06/17 20:45:56 by pszleper         ###   ########.fr       */
+/*   Updated: 2022/06/17 22:07:17 by pszleper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+# define MLX_ERROR -1
 # include <fcntl.h>
 # include <unistd.h>
 # include "libft.h"
 # include "mlx.h"
 
-void	ft_close(void *mlx_ptr, void *mlx_win);
+void	ft_close(void **mlx_ptr, void **mlx_win);
 
 #endif
